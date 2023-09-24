9b3a45ce730abdb08e9962414a90b2deffff3d02
Ricardo Delfin <me@rdelfin.com>
2023-09-25 00:30:44 +0100

    Cambiar a cuadricula

--- imagenes.py
+++ imagenes.py
@@ -7,10 +7,9 @@ pixeles = np.ones((500, 500))
 
 for x in range(500):
     for y in range(500):
-        pixeles[y][x] = (255/2)*math.tan(x*2*math.pi/100)+255/2
-
-
-print(pixeles)
+        y_factor = math.sin(y * 2 * math.pi / 100)
+        x_factor = math.sin(x * 2 * math.pi / 100)
+        pixeles[y][x] = (255 / 2) * (y_factor * x_factor + 1)
 
 img = Image.fromarray(pixeles)
 img.show()
